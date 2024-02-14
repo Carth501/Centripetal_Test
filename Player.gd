f4402a5e549e6bf7368a84eb078aa4f6149991b2
Carth501 <carth501@gmail.com>
2024-02-13 21:18:56 -0500

    fix movement

--- Player.gd
+++ Player.gd
@@ -19,20 +19,15 @@ func _physics_process(delta):
 	set_up_direction(up_vector)
 	var angle = Vector2(position.x, position.y).angle()
 	set_rotation(Vector3(0, 0, angle + deg_to_rad(90)))
-	var subjective_gravity = -up_vector / 2 * delta
+	apply_controls()
+	var subjective_gravity = -up_vector * delta
 	up_vector_display.text += str("\n", subjective_gravity)
 	velocity.y += subjective_gravity.y
-	if(is_on_floor()):
-		var input = Input.get_vector("left", "right", "forward", "backward")
-		var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
-		velocity.x = movement_dir.x * speed
-		velocity.z = movement_dir.z * speed
+	velocity.x += subjective_gravity.x
 	
 	up_vector_display.text += str("\nis_on_floor ", is_on_floor())
 
 	move_and_slide()
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
-		velocity.y = jump_speed
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
@@ -43,6 +38,21 @@ func _input(event):
 		camera_pivot.rotate_y(-event.relative.x * mouse_sensitivity)
 		camera.rotate_x(-event.relative.y * mouse_sensitivity)
 		camera.rotation.x = clampf(camera.rotation.x, -deg_to_rad(85), deg_to_rad(80))
+
+func apply_controls():
+	if(is_on_floor()):
+		var input = Input.get_vector("left", "right", "forward", "backward")
+		var movement_dir = basis * camera_pivot.basis * Vector3(input.x, 0, input.y)
+		up_vector_display.text += str("\n", movement_dir)
+		velocity.x = movement_dir.x * speed
+		velocity.y = movement_dir.y * speed
+		velocity.z = movement_dir.z * speed
+		if Input.is_action_just_pressed("jump"):
+			var jump_dir = basis * Vector3(0, jump_speed, 0)
+			velocity.x += jump_dir.x
+			velocity.y += jump_dir.y
+	else:
+		up_vector_display.text += str("\n", Vector3.ZERO)
 
 func slight_drift() -> Vector3:
 	var rnd = RandomNumberGenerator.new()
